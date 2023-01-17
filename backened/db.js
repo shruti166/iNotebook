082438a7c54e93b7dcdab40b1a3bcd39ebf7c343
shruti166 <shruti.chemistry.lm@gmail.com>
2023-01-17 23:37:35 +0530

    backened in progress

---
+++ backened/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connect = () => {
+  mongoose.connect(
+    "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.1",
+    () => {
+      console.log("connected to db");
+    }
+  );
+};
+
+module.exports = connect;
---
+++ backened/index.js
@@ -0,0 +1,14 @@
+const connect = require("./db.js");
+const express = require("express");
+connect();
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello shruti");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});

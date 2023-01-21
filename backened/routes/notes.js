2c1044d290c49981372ef61d1354ef35de484bbb
shruti166 <shruti.chemistry.lm@gmail.com>
2023-01-22 00:00:47 +0530

    added

--- backened/routes/notes.js
+++ backened/routes/notes.js
@@ -1,1 +1,12 @@
 const express = require('express');
+const {Notes} = require("../models/Notes");
+const notesRouter = express.Router();
+
+notesRouter.get("/post", async(req, res) => {
+    try {
+        const notes = await Notes.find();
+        res.send(notes);
+    } catch(err) {
+
+    }
+}) 

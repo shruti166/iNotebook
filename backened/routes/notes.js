06fb2873220f310d3d50d8bfadc372029050fb66
shruti166 <shruti.chemistry.lm@gmail.com>
2023-01-22 20:15:40 +0530

    added

--- backened/routes/notes.js
+++ backened/routes/notes.js
@@ -40,4 +40,23 @@ notesRouter.patch("/update/:id", async(req, res) => {
     }
 })
 
+
+notesRouter.delete("/delete/:id",async (req,res)=>{
+    const id=req.params.id 
+    const note=await NoteModel.findOne({"_id":id})
+    const userID_in_note=note.userID
+    const userID_making_req=req.body.userID
+    try{
+        if(userID_making_req!==userID_in_note){
+            res.send({"msg":"You are not authorized"})
+        } else {
+            await NoteModel.findByIdAndDelete({"_id":id})
+            res.send("Deleted the note")
+        }
+        
+    }catch(err){
+        console.log(err)
+        res.send({"msg":"Something went wrong"})
+    }
+})
 module.exports = notesRouter ;
